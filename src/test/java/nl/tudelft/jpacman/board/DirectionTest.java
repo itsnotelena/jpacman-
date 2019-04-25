5c6b10290afd3a2dae07603a5b7fc349d1f2fe8f
Elena Ibanez <>
2019-04-25 13:54:27 +0200

    failed test

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -35,7 +35,7 @@ public class DirectionTest {
     @Test
     void testEast() {
         Direction north = Direction.valueOf("EAST");
-        assertThat(north.getDeltaX()).isEqualTo(1);
+        assertThat(north.getDeltaX()).isEqualTo(0);
     }
 
     /**
@@ -44,6 +44,6 @@ public class DirectionTest {
     @Test
     void testWest() {
         Direction north = Direction.valueOf("WEST");
-        assertThat(north.getDeltaX()).isEqualTo(-1);
+        assertThat(north.getDeltaX()).isEqualTo(0);
     }
 }
