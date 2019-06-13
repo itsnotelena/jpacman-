0900f813cc3aed0e69810c21e98610881568839f
Elena Ibanez <>
2019-06-13 21:49:57 +0200

    exercice 2-4, 2-5 update

--- src/default-test/java/nl/tudelft/jpacman/integration/suspension/story2.java
+++ src/default-test/java/nl/tudelft/jpacman/integration/suspension/story2.java
@@ -3,6 +3,7 @@ package nl.tudelft.jpacman.integration.suspension;
 import nl.tudelft.jpacman.Launcher;
 import nl.tudelft.jpacman.board.*;
 import nl.tudelft.jpacman.level.*;
+import nl.tudelft.jpacman.npc.Ghost;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import sun.launcher.resources.launcher;
@@ -10,6 +11,7 @@ import sun.launcher.resources.launcher;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.util.List;
 
 public class story2 {
 
@@ -75,6 +77,38 @@ public class story2 {
         assertThat(player.getSquare()).isEqualTo(beginning);
     }
 
+
+//    @Test
+//    void playerDies() throws IOException {
+//        launcher.withMapFile("/map_scenario2-4.txt");
+//        launcher.launch();
+//        launcher.getGame().start();
+//
+//        Player player = launcher.getGame().getPlayers().get(0);
+//        Square beginning = player.getSquare();
+//        Square destination = player.getSquare().getSquareAt(Direction.EAST);
+//
+//        assertThat(player.getSquare()).isEqualTo(beginning);
+//
+//        launcher.getGame().move(player,Direction.EAST);
+//        //assertThat(destination.getOccupants()).isEqualTo(ghost);
+//        //player.setKiller(ghost);
+//
+//        //assertThat(player.getKiller()).isEqualTo(ghost);
+//        //assertThat(launcher.getGame().isInProgress()).isFalse();
+//    }
+
+    @Test
+    void playerWin() throws IOException {
+        launcher.withMapFile("/map_scenario2-5.txt");
+        launcher.launch();
+        launcher.getGame().start();
+
+        Player player = launcher.getGame().getPlayers().get(0);
+
+        //the game stop
+        assertThat(launcher.getGame().isInProgress()).isFalse();
+    }
 }
 
 
